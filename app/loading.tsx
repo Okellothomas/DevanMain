b9d27315b0909c2dc67632f923026793a2240a20
Okellothomas <thomasolal33@gmail.com>
2024-01-21 13:33:21 +0300

    add loading.tsx documentation

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,10 +1,12 @@
+import Loader from "./components/container/Loader";
 
-import Loader from "./components/container/Loader"
-
+// Loading component definition
 const Loading = () => {
+    // Return the Loader component
     return (
         <Loader />
-    )
+    );
 }
 
-export default Loading; 
+// Export the Loading component as the default export
+export default Loading;

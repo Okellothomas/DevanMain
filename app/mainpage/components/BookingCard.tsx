f86a8ba8944d61525d9e38372b60e559490ebca0
Okellothomas <thomasolal33@gmail.com>
2024-01-25 01:26:38 +0300

    add changes

--- app/mainpage/components/BookingCard.tsx
+++ app/mainpage/components/BookingCard.tsx
@@ -10,11 +10,26 @@ const BookingCard: React.FC<MenuItemProps> = ({
   icon: IconComponent, // Rename imageSrc to icon and use the imported icon component
 }) => {
   return (
-    <div className='flex flex-row justify-between items-center text-center w-full'>
+    <div className='booking-card grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 2xl:grid-cols-3 items-center text-center w-full rounded-xl'>
+
+    <div className='col-span-1 cursor-pointer group flex flex-col gap-2 w-full'>
     <div
-      className="display-card text-center items-center"
+      className="text-center flex flex-col gap-3 items-center"
       >
-     <div className='text-neutral-500'>
+     <div className='bg-white p-3 rounded-full text-neutral-500'>
+        {IconComponent && <IconComponent size={25} />} {/* Display icon if provided */}
+      </div>
+     <div className='text-black rounded-full font-semibold text-md'>
+      {label}
+     </div>
+      </div>
+     </div>
+
+      
+      <div
+      className="text-center flex flex-col gap-3 items-center"
+      >
+     <div className='bg-white p-3 rounded-full text-neutral-500'>
         {IconComponent && <IconComponent size={25} />} {/* Display icon if provided */}
           </div>
      <div className='text-black font-semibold text-md'>
@@ -23,20 +38,9 @@ const BookingCard: React.FC<MenuItemProps> = ({
       </div>
 
       <div
-      className="display-card text-center items-center"
+      className="text-center flex flex-col gap-3 items-center"
       >
-     <div className='text-neutral-500'>
-        {IconComponent && <IconComponent size={25} />} {/* Display icon if provided */}
-          </div>
-     <div className='text-black font-semibold text-md'>
-      {label}
-     </div>
-      </div>
-
-      <div
-      className="display-card text-center items-center"
-      >
-     <div className='text-neutral-500'>
+     <div className='bg-white p-3 rounded-full text-neutral-500'>
         {IconComponent && <IconComponent size={25} />} {/* Display icon if provided */}
           </div>
      <div className='text-black font-semibold text-md'>

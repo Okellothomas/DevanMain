7294703147960afc931882f3aae422d845ee38ac
Okellothomas <thomasolal33@gmail.com>
2024-02-01 04:23:53 +0300

    add changes

--- app/operator/profile/components/SideBar.tsx
+++ app/operator/profile/components/SideBar.tsx
@@ -3,8 +3,15 @@ import { FaRegUser } from "react-icons/fa";
 import { FaSwatchbook } from "react-icons/fa";
 import { LiaSwatchbookSolid } from "react-icons/lia";
 import { IoBookmarks } from "react-icons/io5";
+import { FaRegAddressBook } from "react-icons/fa6";
+import { ImAddressBook } from "react-icons/im";
+import { PiAddressBookFill } from "react-icons/pi";
+import { LiaAddressBookSolid } from "react-icons/lia";
+import { PiBookmarksSimpleBold } from "react-icons/pi";
 import { PiBookOpenTextBold } from "react-icons/pi";
 import { PiBookOpenTextFill } from "react-icons/pi";
+import { BsBookmarkCheck } from "react-icons/bs";
+import { BiBookmarkAltMinus } from "react-icons/bi";
 import Link from "next/link";
 
 // Define the interface for the Home component props
@@ -29,12 +36,12 @@ const ProfilePage = async () => {
           </div>
           <hr />
           <div className="hover:font-semibold text-md flex cursor-pointer hover:bg-neutral-200 flex-row gap-3 py-4 items-center">
-          <FaSwatchbook className="text-neutral-500" size= {26} /> 
+          <BiBookmarkAltMinus className="text-neutral-500" size= {26} /> 
           <Link href="/operator/myhotels">My Hotels</Link>
           </div>
           <hr />
           <div className="hover:font-semibold text-md flex cursor-pointer hover:bg-neutral-200 flex-row gap-3 py-4 items-center">
-          <FaSwatchbook className="text-neutral-500" size= {26} /> 
+          <PiBookmarksSimpleBold className="text-neutral-500" size= {26} /> 
           <Link href="/operator/myhouses">My Houses</Link>
           </div>
           <hr />
@@ -44,7 +51,7 @@ const ProfilePage = async () => {
           </div>
           <hr />
           <div className="hover:font-semibold text-md flex cursor-pointer hover:bg-neutral-200 flex-row gap-3 py-4 items-center">
-          <IoBookmarks className="text-neutral-500" size={26} /> 
+          <BsBookmarkCheck className="text-neutral-500" size={26} /> 
           <Link href="/operator/mybookedhotels">My Booked Hotels</Link>
           </div>
           <hr />

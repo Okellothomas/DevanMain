cf4024205e3c75d16e34d4369a64b73d085d4b5d
kenwekesa <kenwek1994@gmail.com>
2024-02-09 23:31:48 +0300

    changes to build

--- next.config.js
+++ next.config.js
@@ -44,6 +44,14 @@ const nextConfig = {
             },
         ],
     },
+
+    // typescript: {
+    //     // !! WARN !!
+    //     // Dangerously allow production builds to successfully complete even if
+    //     // your project has type errors.
+    //     // !! WARN !!
+    //     ignoreBuildErrors: true,
+    //   }
 }
 
 module.exports = nextConfig

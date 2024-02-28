466a0a5b0b2d843e03fdf5829b064b023d5893e5
Okellothomas <thomasolal33@gmail.com>
2024-02-29 02:33:11 +0300

    add changes

--- app/admin/myhotels/page.tsx
+++ app/admin/myhotels/page.tsx
@@ -27,7 +27,9 @@ const AdministratorsPage = async ({ searchParams, userParams }: HotelPageProps) 
       currentUser = await getCurrentUser();
     }
 
-    const listings = await getMyListingsHotels({ ...searchParams, userId: currentUser?.id });
+    const listings = await getMyListingsHotels(searchParams);
+
+    // const listings = await getMyListingsHotels({ ...searchParams, userId: currentUser?.id });
 
     // Delete user function
     const handleDeleteUser = async (id: string) => {

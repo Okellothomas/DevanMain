ebc731484eb0595a7299c7215fec2ed9c7e06c14
Okellothomas <thomasolal33@gmail.com>
2024-03-16 16:52:06 +0300

    add changes

--- app/tours/[tourId]/TourClient.tsx
+++ app/tours/[tourId]/TourClient.tsx
@@ -827,9 +827,9 @@ const TourClient: React.FC<TourClientProps> = ({
 
         <div className="flex flex-col px-4 justify-between items-center gap-1">
         {error && <div className="text-red-400 text-sm pt-1">{error}</div>}
-        <div className="flex flex-row items-center mt-2">
+        <div className="flex flex-row w-full items-center mt-2">
             <label htmlFor="guests" className="text-right mr-4 text-gray-700">
-                Number of Tourists:
+                Tourists:
             </label>
             <input
                 id="guests"
